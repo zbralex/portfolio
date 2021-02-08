23a3c5e16d8c8b32fc56c996bf6f55f172df3394
Александр Зубрицкий <5998971@google.com>
2021-02-08 17:18:50 +0300

    add routes module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 import {MainPageComponent} from "./public/pages/main-page/main-page.component";
 
 const routes: Routes = [
-  {path: '', component: MainPageComponent},
-  {path: '**', component: MainPageComponent}
+  {
+    path: '',
+    component: MainPageComponent,
+    loadChildren: () => import('./public/public.module').then(m => m.PublicModule )
+  },
+
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/public/public.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from "@angular/router";
+import {MainPageComponent} from "./pages/main-page/main-page.component";
+
+
+const routes: Routes = [
+  {path: '', component: MainPageComponent},
+  {path: '**', component: MainPageComponent}
+]
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ]
+})
+export class PublicModule { }
