46a17d7104dfcd3d77ab5bed3fc2c5c2cc708f77
Александр Зубрицкий <5998971@google.com>
2021-03-17 15:14:51 +0300

    описание

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,20 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './public/pages/main-page/main-page.component';
-import { SkillsComponent } from './public/components/skills/skills.component';
 import { HeaderComponent } from './public/components/header/header.component';
 import { AboutComponent } from './public/components/about/about.component';
 import { SocialsComponent } from './public/components/socials/socials.component';
 import { MyWorksComponent } from './public/components/my-works/my-works.component';
 import { FooterComponent } from './public/components/footer/footer.component';
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
-    SkillsComponent,
     HeaderComponent,
     AboutComponent,
     SocialsComponent,
